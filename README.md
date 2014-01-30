46bea466d79f934e03ea111ee68ed31b4eb3c108
Paul Hayes <paul.hayes@entropedia.co.uk>
2014-01-30 03:18:34 +0000

    Readme edit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 MultilaterationExample
 ======================
+
+WORK IN PROGRESS
+attempt to rewrite [this example](http://blog.andersen.im/2012/07/signal-emitter-positioning-using-multilateration) in python.
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+###########################
+#
+#   WORK IN PROGRESS
+#   attempt to rewrite [this example](http://blog.andersen.im/2012/07/signal-emitter-positioning-using-multilateration) in python.
+#
+############################
+
 from numpy import *
 from numpy.linalg import *
 
