d8cf4d316bccb3f2671fc84b5f3ff5e189a3d8f2
Paul Hayes <paul.hayes@entropedia.co.uk>
2014-02-04 11:41:27 +0000

    Added utf-8 bom

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-###########################
+﻿###########################
 #
 # Python rewrite of multilateration technique by André Andersen in his [blog post](http://blog.andersen.im/2012/07/signal-emitter-positioning-using-multilateration).
 #
