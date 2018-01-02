b5e40033b8625e69b1b8a694c6641511fc7270a3
Paul Hayes <paul.hayes@entropedia.co.uk>
2018-01-02 11:19:35 +0000

    Matrix assignment fix. Json debug data

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 from numpy import *
 from numpy.linalg import *
-
+import json
 #speed of sound in medium
 v = 3450
 numOfDimensions = 3
@@ -35,10 +35,11 @@ A = zeros([nSensors-1,numOfDimensions])
 b = zeros([nSensors-1,1])
 iRow = 0
 rankA = 0
+
 for i in ijs:
 	for j in ijs:
 		A[iRow,:] = 2*( v*(t[j])*(p[:,i]-p[:,c]).T - v*(t[i])*(p[:,j]-p[:,c]).T )
-		b[iRow,0] = v*(t[i])*(v*v*(t[j])**2-p[:,j].T*p[:,j]) + \
+		b[iRow] = v*(t[i])*(v*v*(t[j])**2-p[:,j].T*p[:,j]) + \
 		(v*(t[i])-v*(t[j]))*p[:,c].T*p[:,c] + \
 		v*(t[j])*(p[:,i].T*p[:,i]-v*v*(t[i])**2)
 		rankA = matrix_rank(A)
@@ -49,6 +50,9 @@ for i in ijs:
 		break
 
 calculatedLocation = asarray( lstsq(A,b)[0] )[:,0]
+
+print json.dumps( A.tolist() )
+print json.dumps( b.tolist() )
 
 print "Emitter location: %s " % emitterLocation
 print "Calculated position of emitter: %s " % calculatedLocation
